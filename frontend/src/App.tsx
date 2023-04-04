49112f91d2ae8a119cc0b7741158de0cbfa6c056
DjazzvdHoogt <djazzdjurny@hotmail.com>
2023-04-04 12:42:13 +0200

    add link to YUP

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -25,7 +25,9 @@ function App() {
                 }}
             >
                 <div className={classes.bodyBackground}>
-                    <div>What is already built:</div>
+                    <div>
+                        <strong>What is already built:</strong>
+                    </div>
                     <div>
                         <ul>
                             <li>UI - We already made all the UI components for you</li>
@@ -33,12 +35,37 @@ function App() {
                             <li>Initialized redux store</li>
                         </ul>
                     </div>
-                    <div>Missing pieces in this application:</div>
+                    <div>
+                        <strong>Special packages we used:</strong>
+                    </div>
+                    <div>
+                        <ul>
+                            <li>
+                                <strong>React-hook-form</strong> - Form management package
+                            </li>
+                            <li>
+                                <strong>Mantine UI</strong> - UI Component library
+                            </li>
+                            <li>
+                                <strong>Redux Toolkit</strong> - Simplified Redux state management
+                            </li>
+                            <li>
+                                <strong>Prettier</strong> - This project supports prettier
+                            </li>
+                        </ul>
+                    </div>
+                    <div style={{ color: 'red' }}>
+                        <strong>Missing pieces in this application:</strong>
+                    </div>
                     <div>
                         <ul>
                             <li>Routing</li>
                             <li>Api calls to populate the application</li>
                             <li>Implementing the Redux store to save the data</li>
+                            <li>
+                                Form validation (<strong>TIP!</strong> react-hook-form integrates
+                                easily with <a href="https://github.com/jquense/yup">Yup</a>)
+                            </li>
                         </ul>
                     </div>
                 </div>
